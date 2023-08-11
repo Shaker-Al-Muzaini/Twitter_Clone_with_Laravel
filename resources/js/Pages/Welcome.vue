374035e021409c58c3aaed0f0fefaa2bda881b8e
1shaker <salmazini@students.iugaza.edu.ps>
2023-08-11 20:13:29 +0300

    commit_1

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+import TwitterLayout from '@/Layouts/Twitter.vue';
+
+
+
+</script>
+
+<template>
+    <Head title="Welcome" />
+    <TwitterLayout/>
+</template>
+
